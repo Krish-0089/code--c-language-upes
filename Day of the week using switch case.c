a4149c88fbcf4aafad4ebf7e437900c1b62661bc
Krish-0089 <kambojkrish941@gmail.com>
2025-08-31 12:46:24 +0530

    Create Day of the week using switch case.c

---
+++ Day of the week using switch case.c
@@ -0,0 +1,45 @@
+Q20: Write a program to display the day of the week based on a number (1–7) using switch-case.
+
+/*
+Sample Test Cases:
+Input 1:
+1
+Output 1:
+Monday
+
+Input 2:
+5
+Output 2:
+Friday
+
+*/
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter the value of a: ");
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:
+        printf("Monday");
+        break;
+        case 2:
+        printf("Tuesday");
+        break;
+        case 3:
+        printf("Wednesday");
+        break;
+        case 4:
+        printf("Thrusday");
+        break;
+        case 5:
+        printf("Friday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+        case 7:
+        printf("Sunday");
+        break;
+    }
+}
