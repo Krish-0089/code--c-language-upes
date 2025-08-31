4ed1f125e1874ecf9020a65cdd7fad709b560c2d
Krish-0089 <kambojkrish941@gmail.com>
2025-08-31 12:48:16 +0530

    Create profit or loss percentage.c

---
+++ profit or loss percentage.c
@@ -0,0 +1,41 @@
+Q22: Write a program to find profit or loss percentage given cost price and selling price.
+
+/*
+Sample Test Cases:
+Input 1:
+1000 1200
+Output 1:
+Profit 20%
+
+Input 2:
+1000 800
+Output 2:
+Loss 20%
+
+Input 3:
+1000 1000
+Output 3:
+No Profit No Loss
+
+*/#include<stdio.h>
+int main(){
+    int cost_price;
+    int selling_price;
+    printf("cost price : ");
+    scanf("%d",&cost_price);
+    printf("selling price : ");
+    scanf("%d",&selling_price);
+    int profit,loss;
+    profit=(selling_price-cost_price)/10;
+    loss=(cost_price-selling_price)/10;
+    if(cost_price<selling_price){
+    printf("profit =%d%%",profit);        
+    }
+    else if(cost_price>selling_price){
+        printf("loss =%d%%",loss);
+    }
+    else{
+        printf("no profit no loss");
+    }
+    return 0;
+}
