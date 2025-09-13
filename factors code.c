d27fc8180b60f07b59d8e086f28d163bb168debb
Krish-0089 <kambojkrish941@gmail.com>
2025-09-13 10:00:44 +0530

    Create factors code.c

---
+++ factors code.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int n;
+printf("Enter your number: ");
+scanf("%d",&n);
+if(n<=0){
+    printf("Enter a positive number");
+}
+else{
+for(int i=1;i<=n;i++){
+    if(n%i==0){
+    printf("%d\t",i);
+    }
+ }
+}
+return 0;
+}
