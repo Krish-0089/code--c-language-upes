fbd16ffe42a75eb0ef3da0e67375ee7ca31da9db
Krish-0089 <kambojkrish941@gmail.com>
2025-09-14 19:13:58 +0530

    Create perfect number code.c

---
+++ perfect number code.c
@@ -0,0 +1,35 @@
+//Q42: Write a program to check if a number is a perfect number.
+
+/*
+Sample Test Cases:
+Input 1:
+6
+Output 1:
+Perfect number
+
+Input 2:
+10
+Output 2:
+Not perfect number
+
+*/
+#include<stdio.h>
+int main(){
+    int num,i,sum=0;
+    printf("Enter your number: ");
+    scanf("%d",&num);
+    
+    for(i=1;i<num;i++){
+       if(num %i==0){
+        sum+=i;
+        
+    }
+}
+if(sum==num){
+    printf("%d is a perfect number",sum);
+}
+else{
+    printf("%d is not a perfect number",sum);
+}
+    return 0;
+}
