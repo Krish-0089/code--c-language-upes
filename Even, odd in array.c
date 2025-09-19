6362aee896479712380fa079d7bc4bd92d316764
Krish-0089 <kambojkrish941@gmail.com>
2025-09-19 10:19:46 +0530

    Create Even, odd in array.c

---
+++ Even, odd in array.c
@@ -0,0 +1,42 @@
+//Q59: Count even and odd numbers in an array.
+
+/*
+Sample Test Cases:
+Input 1:
+6
+1 2 3 4 5 6
+Output 1:
+Even=3, Odd=3
+
+Input 2:
+4
+2 4 6 8
+Output 2:
+Even=4, Odd=0
+
+*/
+#include<stdio.h>
+int main(){
+    int n,i;
+   printf("Enter the size of array: ");
+   scanf("%d",&n);
+   int arr[n];
+   int even=0,odd=0;
+   for(i=0;i<n;i++){
+    printf("Enter the element of array: ",i+1);
+    scanf("%d",&arr[i]);
+
+   }
+   for(i=0;i<n;i++){
+    if(arr[i]%2==0){
+        even++;
+    }
+    else{
+        odd++;
+    }
+    
+   }
+   printf("Even numbers: %d\n",even);
+   printf("Odd numbers: %d\n",odd);
+    return 0;
+}
