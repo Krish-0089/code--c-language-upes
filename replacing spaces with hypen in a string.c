8990264425fdcda2d98a75c4fc015e2b36ecac54
Krish-0089 <kambojkrish941@gmail.com>
2025-10-23 18:19:31 +0530

    Create replacing spaces with hypen in a string.c

---
+++ replacing spaces with hypen in a string.c
@@ -0,0 +1,31 @@
+//Q88: Replace spaces with hyphens in a string.
+
+/*
+Sample Test Cases:a
+Input 1:
+hello world
+Output 1:
+hello-world
+
+*/
+#include <stdio.h>
+
+int main(){
+    char str[100];
+
+    
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            str[i] = '-';
+        }
+    }
+
+    
+    printf("%s", str);
+
+    return 0;
+}
