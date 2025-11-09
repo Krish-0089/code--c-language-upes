84a970e6f7cccb669d507af4c8cce0746e5ef5bc
Krish-0089 <kambojkrish941@gmail.com>
2025-11-09 11:42:27 +0530

    Add program to find most occurring digit in integer
    Implement a program to find the most occurring digit in an integer.

---
+++ most occurring digit in integer code.c
@@ -0,0 +1,44 @@
+//Q64: Find the digit that occurs the most times in an integer number.
+
+/*
+Sample Test Cases:
+Input 1:
+112233
+Output 1:
+1
+
+Input 2:
+887799
+Output 2:
+7
+
+*/
+
+#include <stdio.h>
+int main() {
+    long long num;
+    int freq[10] = {0};  
+    printf("Enter your number:");
+    scanf("%lld", &num);
+
+    while (num > 0) {
+        int digit = num % 10;
+        freq[digit]++;
+        num /= 10;
+    }
+
+    int maxDigit = 0;
+    int maxFreq = freq[0];
+
+    for (int i = 1; i < 10; i++) {
+        if (freq[i] > maxFreq) {
+            maxFreq = freq[i];
+            maxDigit = i;
+        }
+    }
+
+    printf("%d\n", maxDigit);
+    return 0;
+}
+
+
