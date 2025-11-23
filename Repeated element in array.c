b339e1171c144e523bc87daf9a9f363a419de4c1
Krish-0089 <kambojkrish941@gmail.com>
2025-11-24 00:16:26 +0530

    Add program to find repeated element in array
    Implement a program to find the repeated element in an integer array in a single iteration.

---
+++ Repeated element in array.c
@@ -0,0 +1,53 @@
+//Q119: Write a program to take an integer array as input. Only one element will be repeated. Print the repeated element. Try to find the result in one single iteration.
+
+/*
+Sample Test Cases:
+Input 1:
+nums1 = [1,3,3,4]
+Output 1:
+3
+
+Input 2:
+nums1 = [1,2,2]
+Output 2:
+2
+
+Input 3:
+nums1 = [0,4,1,1,5]
+Output 3:
+1
+
+*/  
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    int repeated = -1;
+    int flag = 0; 
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(nums[i] == nums[j]) {
+                repeated = nums[i];
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 1) break; 
+    }
+
+    if(flag == 1)
+        printf("Repeated element: %d\n", repeated);
+    else
+        printf("No repeated element found.\n");
+
+    return 0;
+}
